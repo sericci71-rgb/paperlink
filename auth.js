dda029c929a5259c8f79dc302029f09776f389db
sericci71-rgb <se.ricci71@gmail.com>
2025-10-02 16:23:18 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,12 @@
-const firebaseConfig = { apiKey: "YOUR_API_KEY", authDomain: "YOUR_PROJECT_ID.firebaseapp.com", projectId: "YOUR_PROJECT_ID", storageBucket: "YOUR_PROJECT_ID.appspot.com", messagingSenderId: "YOUR_SENDER_ID", appId: "YOUR_APP_ID" }; firebase.initializeApp(firebaseConfig); const auth = firebase.auth(); function login() { const email = document.getElementById('email').value; const password = document.getElementById('password').value; auth.signInWithEmailAndPassword(email, password) .then(() => { document.getElementById('auth-container').style.display = 'none'; document.getElementById('app').style.display = 'block'; loadLinks(); }); } // Simmetriche funzioni per register(), logout(), ecc...
+const firebaseConfig = { apiKey: "AIzaSyBGETnka8pVkgG7g0zekVUM7X8xLSGj6UE", 
+                        authDomain: "paperlink-6bcbf.firebaseapp.com", 
+                        projectId: "paperlink-6bcbf", 
+                        storageBucket: "paperlink-6bcbf.appspot.com", 
+                        messagingSenderId: "750496264953", appId: "1:750496264953:web:7f850cdb41200df6152828" }; 
+firebase.initializeApp(firebaseConfig); 
+const auth = firebase.auth(); 
+function login() { const email = document.getElementById('email').value; 
+                  const password = document.getElementById('password').value; 
+                  auth.signInWithEmailAndPassword(email, password) .then(() => { document.getElementById('auth-container').style.display = 'none'; 
+                                                                                document.getElementById('app').style.display = 'block'; 
+                                                                                loadLinks(); }); } // Simmetriche funzioni per register(), logout(), ecc...
